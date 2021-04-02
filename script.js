db0436f881348d9fa88dff605f8146be5e2c0200
Gustavo Rodrigues <68555292+JayEsi@users.noreply.github.com>
2021-04-02 02:13:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function add_task(){
+
+    let body = document.body
+    let button = document.createElement("button");
+    let ul = document.createElement("ul");
+    let li = document.createElement("li");
+    let input_value = document.getElementById("task").value;
+    let icon = document.createElement("i");
+    let li_text = document.createTextNode(document.getElementById("task").value);
+    
+    button.addEventListener("click", function remove(){
+        body.removeChild(ul);
+    })
+
+    icon.setAttribute("class", "fas fa-times");
+
+    button.appendChild(icon);
+    li.appendChild(button);
+    li.appendChild(li_text);
+    ul.appendChild(li);
+    
+    if(input_value.length == 0){
+        alert("Insira algo na caixa de texto");
+    }else{
+        body.appendChild(ul);
+        document.getElementById("task").value = null;
+    }
+
+}
